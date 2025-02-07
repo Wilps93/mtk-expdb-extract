e19d16a7e6c67d37ba252d2724584fb38e3d0246
Wilps <demi@wilps.ru>
2025-02-07 21:33:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 
-
 def sanitize_logs(logs):
     log_str = '\n'.join(logs)
     log_str = str.replace(log_str, '\x00', '\n')
@@ -13,42 +12,48 @@ def sanitize_logs(logs):
     del logs
     return logs_out
 
-
 def main():
-    f = open('expdb', 'r', encoding='ISO-8859-1')
-    lines = f.readlines()
+    with open('expdb', 'r', encoding='ISO-8859-1') as f:
+        lines = f.readlines()
+
     dumps = []
-    dump = {}
+    # Initialize dump with both keys to avoid KeyError
+    dump = {'pl_lk': [], 'linux': []}
     reading_pllk = False
     reading_linux = False
+
     for line in lines:
         if "Preloader Start=" in line:
             reading_pllk = True
             reading_linux = False
-            if dump != {}:
+            # If dump already contains data, save it before resetting
+            if dump['pl_lk'] or dump['linux']:
                 dumps.append(dump)
-            dump = {
-                'pl_lk': [],
-                'linux': [],
-            }
+            dump = {'pl_lk': [], 'linux': []}
         elif "[LK]jump to K64" in line:
             reading_pllk = False
             reading_linux = True
+
         if reading_pllk:
             dump['pl_lk'].append(line)
         if reading_linux:
             dump['linux'].append(line)
+
+    # Append the final dump if it contains data
+    if dump['pl_lk'] or dump['linux']:
+        dumps.append(dump)
+
     if os.path.isdir('out'):
         shutil.rmtree('out')
     os.mkdir('out')
-    for i in range(len(dumps)):
-        ddir = 'out/' + str(i+1)
-        os.mkdir(ddir)
-        with open(ddir + '/pl_lk', 'w', encoding='ISO-8859-1') as f:
-            f.writelines(dumps[i]['pl_lk'])
-        with open(ddir + '/linux', 'w', encoding='ISO-8859-1') as f:
-            f.writelines(sanitize_logs(dumps[i]['linux']))
 
+    for i, d in enumerate(dumps):
+        ddir = os.path.join('out', str(i + 1))
+        os.mkdir(ddir)
+        with open(os.path.join(ddir, 'pl_lk'), 'w', encoding='ISO-8859-1') as f:
+            f.writelines(d['pl_lk'])
+        with open(os.path.join(ddir, 'linux'), 'w', encoding='ISO-8859-1') as f:
+            f.writelines(sanitize_logs(d['linux']))
 
 if __name__ == '__main__':
     main()
